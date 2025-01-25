d08f4814e50fbb54a8aae87d3afd0924c5d88f00
stbozov <stefan.bozov@outlook.com>
2025-01-25 16:33:48 +0200

    refactoring

--- src/code/main.ts
+++ src/code/main.ts
@@ -1,72 +1,11 @@
 import { CanvasBuilder } from "./builder.js";
-import { CrosslyCanvasConfig } from "./canvas/types.js";
-import { CueCanvasConfig, GridCanvasConfig, StitchCanvasConfig } from "./canvas/types.js";
-
-const gridConfig: GridCanvasConfig = {
-    columns: 30,
-    rows: 30,
-    spacing: {
-        value: 25,
-        zoomStep: 0.5
-    },
-    dot: {
-        color: "#9fa19f",
-        radius: {
-            value: 2,
-            zoomStep: 0.1
-        }
-    },
-    line: {
-        color: "#d2d4d2",
-        width: {
-            value: 1,
-            zoomStep: 0.05
-        }
-    }
-};
-
-const stitchConfig: StitchCanvasConfig = {
-    dot: {
-        color: "gray",
-        radius: {
-            value: 2,
-            zoomStep: 0.1
-        },
-    },
-    line: {
-        color: "gray",
-        width: {
-            value: 6,
-            zoomStep: 0.30
-        }
-    }
-}
-
-const cueConfig: CueCanvasConfig = {
-    dot: {
-        color: "gray",
-        radius: {
-            value: 4,
-            zoomStep: 0.1
-        },
-    },
-    line: {
-        color: "gray",
-        width: {
-            value: 2,
-            zoomStep: 0.05
-        }
-    }
-}
-
-const canvasConfig: CrosslyCanvasConfig = {
-    grid: gridConfig,
-    stitch: stitchConfig,
-    cue: cueConfig
-}
+import { ConfigFactory } from "./config/default.js";
 
 const canvasBuilder = new CanvasBuilder();
-canvasBuilder.withConfig(canvasConfig);
+
+const configFactory = new ConfigFactory();
+const config = configFactory.create();
+canvasBuilder.withConfig(config);
 
 const canvas = canvasBuilder.build();
 canvas.draw();
