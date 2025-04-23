3109227ffa6dcb39b8f6b6d6e9451ea061a5b97b
stbozov <stefan.bozov@outlook.com>
2025-04-23 11:17:42 +0300

    crossly canvas observer implemented

--- src/code/observer/canvas.ts
+++ src/code/observer/canvas.ts
@@ -1,40 +1,67 @@
 import { Base } from "../base.js";
 import assert from "../asserts/assert.js";
 import { VoidUnsubscribe } from "../types.js";
+import { Messaging1 } from "../messaging/impl.js";
 import { ICrosslyCanvas } from "../canvas/types.js";
-import { ChangeListener, ICrosslyCanvasObserver } from "./types.js";
+import { IMessaging1 } from "../messaging/types.js";
+import { IThreadPath } from "../canvas/utilities/arrays/types.js";
 import { ChangeFabricEvent, ChangeStitchPatternEvent } from "../canvas/virtual/types.js";
+import { ChangeEvent, ChangeListener, CrosslyCanvasProject, ICrosslyCanvasObserver } from "./types.js";
 
 export class CrosslyCanvasObserver extends Base implements ICrosslyCanvasObserver {
+    private readonly messaging: IMessaging1<ChangeEvent>;
     private readonly canvas: ICrosslyCanvas;
+
+    private project: CrosslyCanvasProject;
 
     constructor(canvas: ICrosslyCanvas) {
         super(CrosslyCanvasObserver.name);
 
         this.canvas = canvas;
+        this.messaging = new Messaging1();
+
+        const name = this.canvas.config.name;
+        assert.defined(name, "name");
+
+        const fabric = this.canvas.config.fabric;
+        assert.defined(fabric, "FabricCanvasConfig");
+
+        const pattern = new Array<IThreadPath>();
+        this.project = { name, fabric, pattern };
+
         this.subscribe();
     }
 
     public onChange(listener: ChangeListener): VoidUnsubscribe {
         super.ensureAlive();
-
-        throw new Error("Method not implemented.");
+        return this.messaging.listenOnChannel1(listener);
     }
 
     private handleChangeFabric(event: ChangeFabricEvent): void {
         super.ensureAlive();
-        assert.defined(event, "ChangeFabricEvent");
-        assert.defined(event.fabric, "Fabric");
 
-        throw new Error("Method not implemented.");
+        const fabric = event.fabric;
+        assert.defined(fabric, "Fabric");
+        assert.defined(this.project, "project");
+
+        this.project.fabric = fabric;
+        this.invokeProjectChange(this.project);
     }
 
     private handleChangeStitchPattern(event: ChangeStitchPatternEvent): void {
         super.ensureAlive();
-        assert.defined(event, "ChangeStitchPatternEvent");
-        assert.defined(event.pattern, "StitchPattern");
 
-        throw new Error("Method not implemented.");
+        const pattern = event.pattern;
+        assert.defined(event.pattern, "StitchPattern");
+        assert.defined(this.project, "project");
+
+        this.project.pattern = pattern;
+        this.invokeProjectChange(this.project);
+    }
+
+    private invokeProjectChange(project: CrosslyCanvasProject): void {
+        const event = { project };
+        this.messaging.sendToChannel1(event);
     }
 
     private subscribe() {
--- src/code/observer/types.ts
+++ src/code/observer/types.ts
@@ -1,11 +1,11 @@
+import { StitchPattern } from "../canvas/types.js";
 import { Fabric } from "../canvas/virtual/types.js";
-import { Pattern } from "../serialization/types.js";
 import { Listener, VoidUnsubscribe } from "../types.js";
 
 export interface ICrosslyCanvasObserver {
     onChange(listener: ChangeListener): VoidUnsubscribe;
 }
 
-export type CrosslyCanvas = { fabric: Fabric; pattern: Pattern; };
-export type ChangeEvent = { event: CrosslyCanvas; }
+export type CrosslyCanvasProject = { name: string, fabric: Fabric; pattern: StitchPattern; };
+export type ChangeEvent = { project: CrosslyCanvasProject; }
 export type ChangeListener = Listener<ChangeEvent>;
