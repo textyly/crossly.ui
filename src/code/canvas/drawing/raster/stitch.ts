2c9d38d074ea624503f6b41720515f58fd8e317c
stbozov <stefan.bozov@outlook.com>
2025-05-20 16:15:17 +0300

    start drawing stitches on the back

--- src/code/canvas/drawing/raster/stitch.ts
+++ src/code/canvas/drawing/raster/stitch.ts
@@ -4,9 +4,9 @@ import { RasterDrawingCanvas } from "./raster.js";
 import { ShapeDrawing } from "./primitives/shape.js";
 import { IStitchRasterDrawingCanvas } from "../types.js";
 import { RasterLineDrawing } from "./primitives/line.js";
-import { StitchPattern, StitchSegment } from "../../types.js";
 import { RasterPolygonDrawing } from "./primitives/polygon.js";
 import { RasterRectangleDrawing } from "./primitives/rectangle.js";
+import { CanvasSide, StitchPattern, StitchSegment } from "../../types.js";
 
 export class StitchRasterDrawingCanvas extends RasterDrawingCanvas implements IStitchRasterDrawingCanvas {
     private shape: ShapeDrawing;
@@ -26,6 +26,7 @@ export class StitchRasterDrawingCanvas extends RasterDrawingCanvas implements IS
 
         const width = segment.width;
         const color = segment.color;
+        const side = segment.side;
         const fromX = segment.from.x - this.bounds.left;
         const fromY = segment.from.y - this.bounds.top;
         const toX = segment.to.x - this.bounds.left;
@@ -33,7 +34,11 @@ export class StitchRasterDrawingCanvas extends RasterDrawingCanvas implements IS
 
         this.shape.draw(density, path, fromX, fromY, toX, toY, width);
 
-        this.drawPath(path, color);
+        if (side === CanvasSide.Front) {
+            this.drawPath(path, color);
+        } else {
+            this.drawBackPath(path, color);
+        }
     }
 
     public drawLines(pattern: StitchPattern, density: Density): void {
@@ -47,7 +52,10 @@ export class StitchRasterDrawingCanvas extends RasterDrawingCanvas implements IS
     private drawLinesCore(pattern: StitchPattern, density: Density): void {
         // CPU, GPU, memory and GC intensive code, do not extract in multiple methods!!!
         let previousThreadColor = pattern[0].color;
-        let path = this.createPath();
+
+        let frontPath = this.createPath();
+        let backPath = this.createPath();
+
         const left = this.bounds.left;
         const top = this.bounds.top;
 
@@ -59,35 +67,39 @@ export class StitchRasterDrawingCanvas extends RasterDrawingCanvas implements IS
             const positionsY = currentThread.positionsY;
 
             if (currentThread.color !== previousThreadColor) {
-                this.drawPath(path, previousThreadColor);
-                path = this.createPath();
+                this.drawPath(frontPath, previousThreadColor);
+                this.drawBackPath(backPath, previousThreadColor);
+
+                frontPath = this.createPath();
+                backPath = this.createPath();
             }
 
             for (let dotIdx = 1; dotIdx < currentThread.length; dotIdx++) {
 
-                // filter out back stitches as well as stitches positioned out of the visible area
-                if ((dotIdx % 2 !== 0)) {
+                // if `from` or `to` visible then draw the line (segment)
+                const isSegmentVisible = visibilities[dotIdx - 1] || visibilities[dotIdx];
 
-                    // if `from` or `to` visible then draw the line (segment)
-                    const isSegmentVisible = visibilities[dotIdx - 1] || visibilities[dotIdx];
+                if (isSegmentVisible) {
+                    // filter out back stitches as well as stitches positioned out of the visible area
+                    const path = (dotIdx % 2 !== 0) ? frontPath : backPath;
 
-                    if (isSegmentVisible) {
-                        this.shape.draw(
-                            density,
-                            path,
-                            positionsX[dotIdx - 1] - left,
-                            positionsY[dotIdx - 1] - top,
-                            positionsX[dotIdx] - left,
-                            positionsY[dotIdx] - top,
-                            currentThread.zoomedWidth);
-                    }
+                    this.shape.draw(
+                        density,
+                        path,
+                        positionsX[dotIdx - 1] - left,
+                        positionsY[dotIdx - 1] - top,
+                        positionsX[dotIdx] - left,
+                        positionsY[dotIdx] - top,
+                        currentThread.zoomedWidth);
                 }
             }
 
             previousThreadColor = currentThread.color;
         }
 
-        this.drawPath(path, previousThreadColor);
+        this.drawPath(frontPath, previousThreadColor);
+        this.drawBackPath(backPath, previousThreadColor);
+
     }
 
     private createPath(): Path2D {
@@ -96,18 +108,18 @@ export class StitchRasterDrawingCanvas extends RasterDrawingCanvas implements IS
     }
 
     private drawPath(path: Path2D, color: string): void {
-        this.rasterCanvasContext.strokeStyle = color;
-        this.rasterCanvasContext.stroke(path);
-
-        this.rasterCanvasContext.fillStyle = color;
-        this.rasterCanvasContext.fill(path);
+        this.drawPathCore(path, color, this.rasterCanvasContext);
     }
 
     private drawBackPath(path: Path2D, color: string): void {
-        this.backRasterCanvasContext.strokeStyle = color;
-        this.backRasterCanvasContext.stroke(path);
+        this.drawPathCore(path, color, this.backRasterCanvasContext);
+    }
 
-        this.backRasterCanvasContext.fillStyle = color;
-        this.backRasterCanvasContext.fill(path);
+    private drawPathCore(path: Path2D, color: string, canvasContext: CanvasRenderingContext2D): void {
+        canvasContext.strokeStyle = color;
+        canvasContext.stroke(path);
+
+        canvasContext.fillStyle = color;
+        canvasContext.fill(path);
     }
 }
