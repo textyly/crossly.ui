ae99251af1bc085730f26a3d13ff0887b60eea84
stbozov <stefan.bozov@outlook.com>
2025-06-26 10:33:39 +0300

    refactoring

--- src/code/menu/menus/palette.ts
+++ src/code/menu/menus/palette.ts
@@ -6,28 +6,28 @@ import { IMessaging1 } from "../../messaging/types.js";
 import { ChangeThreadEvent, ChangeThreadListener, Color, Colors, IPaletteMenu } from "./types.js";
 
 export class PaletteMenu extends Base implements IPaletteMenu {
-    private messaging: IMessaging1<ChangeThreadEvent>;
+    private readonly buttonClassName = "color-button";
 
+    private readonly messaging: IMessaging1<ChangeThreadEvent>;
     private readonly container: Element;
-    private readonly activeColors: Array<Color>;
-    private readonly colorButtonsListeners: Array<(event: Event) => void>;
+
+    private readonly buttons: Array<Element>;
+    private readonly buttonsListeners: Array<(event: Event) => void>;
 
     constructor(container: Element) {
         super(PaletteMenu.name);
 
         this.container = container;
 
-        this.colorButtonsListeners = [];
+        this.buttonsListeners = [];
         this.messaging = new Messaging1();
 
-        const paletteMenu = this.getPaletteMenu(this.container);
-        const buttons = this.getColorButtons(paletteMenu);
+        let defaultColors = ["#111e6a", "#a9cdd6", "#355f0d", "#cf0013", "#f5e500"]; // TODO: retrieve from a service
+        defaultColors = defaultColors.map((color) => this.normalizeColor(color));
 
-        this.activeColors = buttons
-            .map((button) => this.getButtonColor(button))
-            .map((color) => this.normalizeColor(color));
-
-        this.subscribeButtons(buttons);
+        this.buttons = this.createButtons(defaultColors);
+        this.insertButtons(container, this.buttons);
+        this.subscribeButtons(this.buttons);
     }
 
     public onChangeThread(listener: ChangeThreadListener): VoidUnsubscribe {
@@ -37,23 +37,8 @@ export class PaletteMenu extends Base implements IPaletteMenu {
     public add(colors: Colors): void {
         assert.greaterThanZero(colors.length, "colors.length");
 
-        let uniqueColors = this.getUniqueColors(colors);
-        assert.greaterThanZero(uniqueColors.length, "uniqueColors.length");
-
-        uniqueColors.forEach((color) => {
-            let normalized = this.normalizeColor(color);
-
-            if (!this.activeColors.find((ac) => ac === normalized)) {
-                this.activeColors.push(normalized);
-
-                const newButton = this.createButton(normalized);
-                const addButton = this.getAddButton(this.container);
-                const paletteMenu = this.getPaletteMenu(this.container);
-
-                paletteMenu.insertBefore(newButton, addButton);
-                this.subscribeButton(newButton);
-            }
-        });
+        const uniqueColors = this.getUniqueColors(colors);
+        this.addCore(uniqueColors);
     }
 
     public override dispose(): void {
@@ -61,8 +46,46 @@ export class PaletteMenu extends Base implements IPaletteMenu {
         super.dispose();
     }
 
+    private addCore(colors: Colors): void {
+        colors.forEach((color) => {
+            const normalizedColor = this.normalizeColor(color);
+            const buttonsColors = this.buttons.map((button) => this.getElementColor(button));
+
+            if (!buttonsColors.find((ac) => ac === normalizedColor)) {
+                const button = this.createButton(normalizedColor);
+                this.insertButtons(this.container, [button]);
+                this.subscribeButton(button);
+                this.buttons.push(button);
+            }
+        });
+    }
+
+    private insertButtons(container: Element, buttons: Array<Element>): void {
+        const paletteMenu = this.getPaletteMenu(container);
+        const addButton = this.getAddButton(container);
+        buttons.forEach((b) => paletteMenu.insertBefore(b, addButton));
+    }
+
+    private createButtons(colors: Colors): Array<Element> {
+        const buttons: Array<Element> = [];
+
+        colors.forEach((color) => {
+            const button = this.createButton(color);
+            buttons.push(button);
+        });
+
+        return buttons;
+    }
+
+    private createButton(color: Color): Element {
+        const button = document.createElement("button");
+        button.classList.add(this.buttonClassName);
+        button.style.backgroundColor = color;
+        return button;
+    }
+
     private getPaletteMenu(container: Element): Element {
-        const paletteElement = container.querySelector('#color-buttons');
+        const paletteElement = container.querySelector('#color-buttons-container');
         assert.defined(paletteElement, "paletteElement");
         return paletteElement;
     }
@@ -73,37 +96,20 @@ export class PaletteMenu extends Base implements IPaletteMenu {
         return addElement;
     }
 
-    private getColorButtons(container: Element): Array<Element> {
-        const elements = container.querySelectorAll(".color-button");
-        assert.greaterThanZero(elements.length, "buttons.length");
-        return [...elements];
-    }
-
-    private createButton(color: Color): HTMLButtonElement {
-        const button = document.createElement("button");
-
-        // TODO:  color-button must be extracted from the existing buttons
-        button.classList.add("color-button");
-
-        button.style.backgroundColor = color;
-
-        return button;
-    }
-
     private getUniqueColors(colors: Array<string>): Array<string> {
-        const unique = new Set<string>();
+        const unique: Array<string> = []; // keep the order of the colors, do not use Set<string>
 
         colors.forEach((color) => {
-            const has = unique.has(color);
-            if (!has) {
-                unique.add(color);
+            const duplicate = unique.find((c) => c === color);
+            if (!duplicate) {
+                unique.push(color);
             }
         });
 
-        return [...unique];
+        return unique;
     }
 
-    private getButtonColor(element: Element): string {
+    private getElementColor(element: Element): string {
         return getComputedStyle(element).backgroundColor;
     }
 
@@ -117,8 +123,12 @@ export class PaletteMenu extends Base implements IPaletteMenu {
         return computedColor;
     }
 
-    private getElementColor(element: Element): string {
-        return getComputedStyle(element).backgroundColor;
+    private handleChangeColor(event: Event): void {
+        const element = event.currentTarget as Element;
+        assert.defined(element, "target");
+
+        const color = this.getElementColor(element);
+        this.invokeChangeThread(color);
     }
 
     private subscribeButtons(buttons: Array<Element>): void {
@@ -128,34 +138,19 @@ export class PaletteMenu extends Base implements IPaletteMenu {
     private subscribeButton(button: Element): void {
         const handler = this.handleChangeColor.bind(this);
         button.addEventListener("click", handler);
-        this.colorButtonsListeners.push(handler);
+        this.buttonsListeners.push(handler);
     }
 
     private unsubscribeButtons(): void {
-        const buttons = this.container.querySelectorAll("button");
-
-        assert.defined(buttons, "buttons");
-        assert.greaterThanZero(buttons?.length, "buttons.length");
-
-        assert.defined(this.colorButtonsListeners, "changeColorListeners");
-
-        for (let index = 0; index < buttons.length; index++) {
-            const button = buttons[index];
-            const listener = this.colorButtonsListeners[index];
+        for (let index = 0; index < this.buttons.length; index++) {
+            const button = this.buttons[index];
+            const listener = this.buttonsListeners[index];
             button.removeEventListener("click", listener);
         }
     }
 
-    private handleChangeColor(event: Event): void {
-        const element = event.currentTarget as Element;
-        assert.defined(element, "target");
-
-        const color = this.getElementColor(element);
-        this.invokeChangeThread(color);
-    }
-
     private invokeChangeThread(color: Color): void {
-        const event = { name: "test", color, width: 12 }; // TODO: 
+        const event = { name: "test", color, width: 12 }; // TODO: !!!
         this.messaging.sendToChannel1(event);
     }
 }
